904041cc3c39dcc287825d1379ca418ac312a679
Rohit Kumar <rr7433446@gmail.com>
2022-10-15 16:16:00 +0530

    Add solution for problem `A - Way Too Long Words` Link: https://codeforces.com/contest/71/problem/A Tags: strings, *800 Ref: https://codeforces.com/contest/71/submission/176279772

---
+++ codeforces/71/A.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long int ll;
+typedef long double   lld;
+const ll mod=1e9+7;
+#define endl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define print(a) for(auto i:a)cout<<i<<" ";cout<<endl;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define read(a) for(auto &i:a)cin>>i
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(x) ll((x.size()))
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+int main(){
+	int t;
+	cin>>t;
+
+	while(t--){
+		string s;
+	cin>>s;
+
+	int n = s.size();
+	if(n<=10){
+		cout<<s<<endl;
+	}
+	else{
+		string ans = "";
+		ans += s[0];
+		ans += to_string(n-2);
+		ans +=s[n-1];
+		cout<<ans<<endl;
+	}
+	}
+	
+#ifndef ONLINE_JUDGE
+   freopen( "input.txt" , "r", stdin);
+   freopen("output.txt" , "w", stdout);
+#endif
+ return 0;
+}
