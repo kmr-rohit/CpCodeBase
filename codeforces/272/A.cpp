66904b360ab3980957cefb5835e8f23ae6446604
Rohit Kumar <rr7433446@gmail.com>
2022-10-28 21:00:00 +0530

    Add solution for problem `A - Dima and Friends` Link: https://codeforces.com/contest/272/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/272/submission/178244303

---
+++ codeforces/272/A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef  long long int ll;
+typedef long double   lld;
+const ll mod=1e9+7;
+#define endl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define print(a) for(auto i:a)cout<<i<<" ";cout<<endl;
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define read(a) for(auto &i:a)cin>>i
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(x) ll((x.size()))
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+int main(){
+
+	int n;
+	cin>>n;
+	int sum = 0;
+	for(int i = 0;i<n;i++){
+		int temp;
+		cin>>temp;
+		sum += temp;
+	}
+	int cnt = 0;
+	for(int i =1;i<=5;i++){
+		if((sum+i)%(n+1) != 1){
+			cnt++;
+		}
+	}
+	cout<<cnt<<endl;
+#ifndef ONLINE_JUDGE
+   freopen( "input.txt" , "r", stdin);
+   freopen("output.txt" , "w", stdout);
+#endif
+ return 0;
+}
