e7fd29e9d790bd3123edb3f8d659111f7a1458de
Rohit Kumar <rr7433446@gmail.com>
2023-10-06 23:03:00 +0530

    Add solution for problem `D - Odd Queries` Link: https://codeforces.com/contest/1807/problem/D Tags: data structures, implementation, *900 Ref: https://codeforces.com/contest/1807/submission/226913178

--- codeforces/1807/D.cpp
+++ codeforces/1807/D.cpp
@@ -1,31 +1,33 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-   int t;
-   cin>>t;
-   while(t--)
-   {
-    int n,q;
-    cin>>n>>q;
-    long long pr[n+1];
-    pr[0]=0;
-    for(int i=1;i<n+1;i++){
-        cin>>pr[i];
-        pr[i]=((pr[i])+pr[i-1]);
 
-    }
-    while(q--)
-    {
-    int l,r;
-    long long k;
-    cin>>l>>r>>k;
-
-    k=k;
-    long long s=pr[n]-(pr[r]-pr[l-1])+k*(r-l+1);
-    if(s%2==1) {cout<<"YES"<<endl;}
-    else{cout<<"NO"<<endl;}
-    }
-   } 
-    
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,q;
+        cin>>n>>q;
+        vector<int> arr(n+1);
+        int sum = 0;
+        for(int i =1;i<=n;i++){
+            cin>>arr[i];
+            sum+=arr[i];
+        }
+        vector<int>ps(n+1, 0);
+        ps[0] = arr[0];
+        for(int i = 1;i<=n;i++){
+            ps[i] = ps[i-1] + arr[i];
+        }
+        while(q--){
+            int l , r , k;
+            cin>>l>>r>>k;
+            long long tempsum = ps[n] - (ps[r] - ps[l-1]) + (r-l + 1)*k;
+            if(tempsum%2){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+        }
+   }
 }
